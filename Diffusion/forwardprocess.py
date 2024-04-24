9c42549cb0be8fbe9ca5201df0ef365bae84b526
Aarushi <anu@Aarushis-MacBook-Pro.local>
2024-04-24 22:09:47 +0530

    Random normal shape

--- Diffusion/forwardprocess.py
+++ Diffusion/forwardprocess.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 
-class ForwardProcess(tf.keras.Model):
+class ForwardProcess:
 
     def __init__(self, betas):
         super().__init__()
@@ -8,8 +8,8 @@ class ForwardProcess(tf.keras.Model):
         self.alphas = 1. - betas
         self.alpha_bar = tf.math.cumprod(self.alphas,axis=-1)
 
-    def call(self, x_0, t):
-        eps_0 = tf.random.normal(x_0)
+    def get_x_t(self, x_0, t):
+        eps_0 = tf.random.normal(tf.shape(x_0))
         alpha_bar = self.alpha_bar[t, None]
         mean = tf.multiply( tf.sqrt( alpha_bar ), x_0)
         std = tf.sqrt( 1 - alpha_bar)
